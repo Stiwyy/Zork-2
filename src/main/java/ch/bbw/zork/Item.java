309de244576402cd91a54a5decbde2b160bc8a38
Stiwyy <andrin.renggli@lernende.bbw.ch>
2024-10-30 08:52:04 +0100

    Test

--- src/main/java/ch/bbw/zork/Item.java
+++ src/main/java/ch/bbw/zork/Item.java
@@ -1,4 +1,25 @@
 package ch.bbw.zork;
 
 public class Item {
+    private String name;
+    private String description;
+    private String room;
+
+    public Item(String name, String description, String room) {
+        this.name = name;
+        this.description = description;
+        this.room = room;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getRoom() {
+        return room;
+    }
 }
